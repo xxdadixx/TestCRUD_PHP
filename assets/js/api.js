54c92ddf98fa5a2655a34412045f7b492cdc5299
xxdadixx <sanshop4@hotmail.com>
2026-01-17 22:03:23 +0700

    Add api.js to define API endpoints

---
+++ assets/js/api.js
@@ -0,0 +1,9 @@
+// assets/js/api.js
+window.API = {
+    customer: {
+        store:  `${window.APP_BASE_URL}/customers/api/store.php`,
+        update: `${window.APP_BASE_URL}/customers/api/update.php`,
+        delete: `${window.APP_BASE_URL}/customers/api/delete.php`,
+        show:   `${window.APP_BASE_URL}/customers/api/show.php`
+    }
+};
